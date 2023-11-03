e9f5e6684702afbfe394942a096ed9339cd96673
donniem <donniem@nycstudents.net>
2023-11-02 22:07:25 -0400

    some work

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 b1 = document.getElementById("b1");
 b1.addEventListener("click", popup(1));
-b2 =document.getElementById("b2");
+b2 = document.getElementById("b2");
 b2.addEventListener("click", popup(2));
 
-function popup(num){
-    if(num=1){
-        
-    }
+function popup(num) {
+  if ((num = 1)) {
+  }
 }
